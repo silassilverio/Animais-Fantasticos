23bc541f7a7296271cf15011ec29ace962e68c2a
Silas Leme Silvério <comikpro22@gmail.com>
2021-08-07 17:57:43 -0300

    acertado o caminho da api

--- js/script.js
+++ js/script.js
@@ -38,5 +38,5 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://www.mercadobitcoin.com.br/api/BTC/ticker', '.btc-preco')
